cc28ccedbeff24cc80af3487fa87cadd3a06170b
michael <michael.shin.hrr@gmail.com>
2018-09-06 20:10:01 -0700

    completed the react env setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # movielist
+## Local Setup
+
+1. `npm install`
+2. `npm start`
+3. from your chrome browser, navigate to localhost:3000
---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        hello this is movie list 
+      </div>
+    )
+  }
+
+
+
+}
+
+export default App;
